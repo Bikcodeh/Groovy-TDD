5fe197ff6d36ce4d951e82c9ce5c0656df0ce549
Victor Hoyos <bikcodeh@gmail.com>
2021-12-01 21:25:29 -0500

    implements test for error case

--- app/src/test/java/petros/efthymiou/groovy/playlist/PlaylistViewModelShould.kt
+++ app/src/test/java/petros/efthymiou/groovy/playlist/PlaylistViewModelShould.kt
@@ -6,16 +6,19 @@ import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import petros.efthymiou.groovy.utils.BaseUnitTest
 import petros.efthymiou.groovy.utils.getValueForTest
+import java.lang.RuntimeException
 
 class PlaylistViewModelShould : BaseUnitTest() {
 
     private val repository: PlaylistRepositoryImpl = mock()
     private val playlist = mock<List<Playlist>>()
     private val expected = Result.success(playlist)
+    private val exception = RuntimeException("Something went wrong")
 
     @Test
     fun getPlaylistFromRepository(): Unit = runBlocking {
@@ -29,6 +32,19 @@ class PlaylistViewModelShould : BaseUnitTest() {
     fun emitsPlaylistFromRepository() = runBlocking {
         val viewModel = mockSuccessfulCase()
         assertEquals(expected, viewModel.playlist.getValueForTest())
+    }
+
+    @Test
+    fun emitErrorWhenReceiveError()  = runBlockingTest {
+        whenever(repository.getPlaylist()).thenReturn(
+            flow {
+                emit(Result.failure<List<Playlist>>(exception))
+            }
+        )
+
+        val viewModel = PlaylistViewModel(repository)
+
+        assertEquals(exception, viewModel.playlist.getValueForTest()!!.exceptionOrNull())
     }
 
     private fun mockSuccessfulCase(): PlaylistViewModel {
