73990cea4fde27002b315bbf01775c2235622541
Victor Hoyos <bikcodeh@gmail.com>
2021-12-03 12:05:27 -0500

    implements idlingresource and remove thread sleep for espresso tests

--- app/src/androidTest/java/petros/efthymiou/groovy/PlaylistFeature.kt
+++ app/src/androidTest/java/petros/efthymiou/groovy/PlaylistFeature.kt
@@ -1,29 +1,20 @@
 package petros.efthymiou.groovy
 
-import android.view.View
-import android.view.ViewGroup
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.rule.ActivityTestRule
 import com.schibsted.spain.barista.assertion.BaristaRecyclerViewAssertions.assertRecyclerViewItemCount
 import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
 import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertNotDisplayed
 import com.schibsted.spain.barista.internal.matcher.DrawableMatcher.Companion.withDrawable
-import org.hamcrest.Description
-import org.hamcrest.Matcher
 import org.hamcrest.Matchers.allOf
-import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
+import petros.efthymiou.groovy.playlist.idlingResource
 
-@RunWith(AndroidJUnit4::class)
-class PlaylistFeature {
+class PlaylistFeature : BaseUITest() {
 
     val scenarioRule = ActivityTestRule(MainActivity::class.java)
         @Rule get
@@ -36,31 +27,47 @@ class PlaylistFeature {
     @Test
     fun displayListOfPlaylist() {
 
-        Thread.sleep(4000)
-
         assertRecyclerViewItemCount(R.id.playlist_list, 10)
 
-        onView(allOf(withId(R.id.tvPlaylistName), isDescendantOfA(nthChildOf(withId(R.id.playlist_list), 0))))
+        onView(
+            allOf(
+                withId(R.id.tvPlaylistName),
+                isDescendantOfA(nthChildOf(withId(R.id.playlist_list), 0))
+            )
+        )
             .check(matches(withText("Hard Rock Cafe")))
             .check(matches(isDisplayed()))
 
-        onView(allOf(withId(R.id.tvPlaylistCategory), isDescendantOfA(nthChildOf(withId(R.id.playlist_list), 0))))
+        onView(
+            allOf(
+                withId(R.id.tvPlaylistCategory),
+                isDescendantOfA(nthChildOf(withId(R.id.playlist_list), 0))
+            )
+        )
             .check(matches(withText("rock")))
             .check(matches(isDisplayed()))
 
-        onView(allOf(withId(R.id.ivPlaylist), isDescendantOfA(nthChildOf(withId(R.id.playlist_list), 0))))
+        onView(
+            allOf(
+                withId(R.id.ivPlaylist),
+                isDescendantOfA(nthChildOf(withId(R.id.playlist_list), 0))
+            )
+        )
             .check(matches(withDrawable(R.mipmap.rock)))
             .check(matches(isDisplayed()))
     }
 
     @Test
     fun displayLoaderWhileFetchingThePlaylist() {
+
+        //Don't block our execution test while app is fetching our data
+        IdlingRegistry.getInstance().unregister(idlingResource)
+
         assertDisplayed(R.id.loader)
     }
 
     @Test
     fun hidesLoader() {
-        Thread.sleep(4000)
         assertNotDisplayed(R.id.loader)
     }
 
@@ -68,32 +75,22 @@ class PlaylistFeature {
     @Test
     fun displayRockImageForRockListItems() {
 
-        Thread.sleep(4000)
-
-        onView(allOf(withId(R.id.ivPlaylist), isDescendantOfA(nthChildOf(withId(R.id.playlist_list), 0))))
+        onView(
+            allOf(
+                withId(R.id.ivPlaylist),
+                isDescendantOfA(nthChildOf(withId(R.id.playlist_list), 0))
+            )
+        )
             .check(matches(withDrawable(R.mipmap.rock)))
             .check(matches(isDisplayed()))
 
-        onView(allOf(withId(R.id.ivPlaylist), isDescendantOfA(nthChildOf(withId(R.id.playlist_list), 3))))
+        onView(
+            allOf(
+                withId(R.id.ivPlaylist),
+                isDescendantOfA(nthChildOf(withId(R.id.playlist_list), 3))
+            )
+        )
             .check(matches(withDrawable(R.mipmap.rock)))
             .check(matches(isDisplayed()))
-    }
-
-    fun nthChildOf(parentMatcher: Matcher<View>, childPosition: Int): Matcher<View> {
-        return object : TypeSafeMatcher<View>() {
-            override fun describeTo(description: Description) {
-                description.appendText("position $childPosition of parent ")
-                parentMatcher.describeTo(description)
-            }
-
-            public override fun matchesSafely(view: View): Boolean {
-                if (view.parent !is ViewGroup) return false
-                val parent = view.parent as ViewGroup
-
-                return (parentMatcher.matches(parent)
-                    && parent.childCount > childPosition
-                    && parent.getChildAt(childPosition) == view)
-            }
-        }
     }
 }
